e735705d19f33bbc6726317a94d8f0b1130555d4
Kinh Hy Chau <73710537+xiaiyi99@users.noreply.github.com>
2021-03-12 10:14:39 -0700

    Update class.js

--- class.js
+++ class.js
@@ -42,7 +42,7 @@ const canada = new Nation('38 millions', '9.98 million square km', '1.
 const usa = new Nation('328 millions', '9.15 million square km', '20.8 trillion' );
 const calgary = new City ('4 millions', '0.66 million square km', '105 billion', '11 percent' );
 const highwood = new Neighborhood ('50,000', '0.02 million square km', '1 billion', '9 percent', '150,000 per year');
-console.log(canada);
+document.write("Canada" canada);
 console.log(usa);
 console.log(calgary);
 console.log(highwood);
